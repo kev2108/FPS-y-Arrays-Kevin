b8ceee878ea3fd67f7a79e849b99a46f545582c9
kev2108 <kevindanieldana@gmail.com>
2024-06-12 23:29:34 -0300

    Initial commit

---
+++ Assets/InteractionArea.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionArea : MonoBehaviour
+{
+    public Text txtPuntaje;
+    public GameObject filete;
+    int puntaje = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        //Obtener nombre de objeto colisionado
+        Debug.Log("Hubo colision con " + other.name);
+        puntaje += (other.gameObject.GetComponent<PickupScript>().valor);
+        txtPuntaje.text = "Puntaje: " + puntaje.ToString();
+        if (other.gameObject.GetComponent<PickupScript>().alimento)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/PickupScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupScript : MonoBehaviour
+{
+    //crear una variable pública que establezca si es alimento o no
+    //crear una variable pública que almacene el valor en una cantidad entera
+
+    public bool alimento;
+    public int valor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
